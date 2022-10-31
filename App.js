9856e1e464d7d1fab7b07211f733ddab31c6c565
David Lozada <davsh47@hotmail.com>
2022-10-31 21:39:25 +0300

    b4

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Navigator from "./src/Navigation";
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth, API, graphqlOperation } from "aws-amplify";
 import awsconfig from "./src/aws-exports";
 import { withAuthenticator } from "aws-amplify-react-native/dist/Auth";
-
+import { useEffect } from "react";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 function App() {
+  useEffect(() => {
+    const syncUser = async () => {
+      /* GET AUTH USER */
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      console.log(authUser);
+      /* QUERY THE DB USING AUTH USER ID (SUB) */
+      const userData = await API.graphql(
+        graphqlOperation(getUser, { id: authUser.attributes.sub })
+      );
+      console.log(userData);
+      /* IF HTERES NO USER IN DB,CREATE ONE */
+      if (userData.data.getUser) {
+        console.log("user already exists");
+        return;
+      }
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.phone_number,
+        status: "Hey,im using wp",
+      };
+      await API.graphql(graphqlOperation(createUser, { input: newUser }));
+    };
+    syncUser();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Navigator />
